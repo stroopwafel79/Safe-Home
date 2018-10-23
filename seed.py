8f5bfc5e4f33971c24b99490714b0436623b19bb
stroopwafel79 <stroopwafel79@gmail.com>
2018-10-23 22:04:04 +0000

    Added pattern matching to search crime data for lat and long

--- seed.py
+++ seed.py
@@ -9,6 +9,7 @@ from model import Crime, Address, CrimeType
 from model import connect_to_db, connect_to_db
 from server import app
 from csv import reader
+import re
 
 def load_crimes():
 	"Load crimes from Oakland Open Data into the database from a CSV file"
@@ -23,15 +24,48 @@ def load_crimes():
 	# Read crime file and import data
 	
 	with open("CrimeWatch_TEST.csv") as f:
-    	next(f)  # ignore header row
+    	# ignore header row
+    	next(f)
+
     	# reader function from csv module knows where end of row is despite
     	# one row taking up 3 lines in the file.
     	crimereader = reader(f, delimiter=',')  
-    
+
+    	# Creates a pattern for latitude and longitude so dirty data can
+    	# be searched more reliably.
+    	pattern = re.compile("-?\d+.\d+") 
+
     	for row in crimereader: 
-        	crime = ', '.join(row).split(', ')
+        	crime_lst = ', '.join(row).split(', ')
+
+        	# Turn date and time in string form to a datetime object
+        	date_time = datetime.strptime(crime_lst[1], "%m/%d/%Y %I:%M:%S %p")
 
         
+        	# check if lat/long included. If not call function to look
+        	# it up based on the address
+        	if pattern.search(crime_lst[-1]):
+        		latitude=pattern.search(crime_lst[9]).group(0),
+        		longitude=pattern.search(crime_lst[10]).group(0)
+        		# call other function ####################
+        	else:
+        		pass
+
+
+
+        	crime = Crime(date_time=date_time, 
+        				  case_num=crime_lst[2],
+        				  description=crime_lst[3].title(),
+        				  beat=crime_type[4]
+        				   )
+
+        	crime_type = CrimeType(crime_lst[0].title())
+
+        	address = Address(street_adrs=crime_lst[5].title(),
+        					  latitude=,
+        					  longitude=
+
+
 
 
 	
