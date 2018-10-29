1be1c12b5a415d6e878ba55482d82ce0e6a0ee1c
stroopwafel79 <stroopwafel79@gmail.com>
2018-10-29 19:00:35 +0000

    Both crime type data and zillow data display separately on page correctly

--- server.py
+++ server.py
@@ -31,85 +31,76 @@ def show_form():
 	"""Show form on homepage for entering search criteria"""
 	return render_template("homepage.html")
 
+
+
 # @app.route('/results')
-# def show_crimes():
-# 	"""Show a list of crimes at that address"""
+# def combine_results():
+# 	"""Combine crimes and zillow data"""
+
+# 	return show_crimes()
+
+@app.route("/results")
+def show_crimes():
+	"""Show a list of crimes at that address"""
 
 	# get the value of the input address from the form
-	# address = request.args.get("address")
+	address = request.args.get("address")
 
-	# # query db, get Address object
-	# adrs_object = Address.query.filter_by(street_adrs=address.title()).first()
-	# # access address_id
-	# adrs_id = adrs_object.address_id
-	# # query db to get list of Crime objects with address_id
-	# # loop over this crimes_lst in jinja
-	# crimes_lst = Crime.query.filter_by(address_id=adrs_id).all()
+	# query db, get Address object
+	adrs_object = Address.query.filter_by(street_adrs=address.title()).first()
+	# access address_id
+	adrs_id = adrs_object.address_id
+	# query db to get list of Crime objects with address_id
+	# loop over this crimes_lst in jinja
+	crimes_lst = Crime.query.filter_by(address_id=adrs_id).all()
 
-	# return render_template("address.html", 
-	# 					   address=address,
-	# 					   crimes_lst=crimes_lst)
+	return render_template("address.html", 
+						   address=address,
+						   crimes_lst=crimes_lst)
 
 
 
-@app.route('/results')
-def get_zillow_data():
-	"""Get info from zillow via api request"""
+# @app.route('/results')
+# def get_zillow_data():
+# 	"""Get info from zillow via api request"""
 
-	# get secret key for zillow api
-	key = environ.get("KEY") 
-
-	street_adrs = "652 54th Street"#request.args.get("address")
-	zipcode = "94609"#request.args.get("zip")
-	citystatezip = " ".join(["Oakland,", "CA", zipcode])
-
-	url = "https://www.zillow.com/webservice/GetSearchResults.htm"
-	payload = {
-		"zws-id": key,
-		"address": street_adrs,
-		"citystatezip": citystatezip
-	}
-
-	# make a request to the api with the payload as parameters. Returns XML.
-	data = requests.get(url, params=payload) # <class requests.models.Response>
-	# print("\nDDDDDDDDDDDDDDDDDDDD")
-	# print(type(data))
-	# pprint(data)
-
-	# turn the XML recieved into a dictionary
-	data_dict = bf.data(fromstring(data.text)) # <class dict>
+# 	# get secret key for zillow api
+# 	key = environ.get("KEY")
 	
-	# first key of data_dict
-	key1 = "{http://www.zillow.com/static/xsd/SearchResults.xsd}searchresults"
-	results = data_dict[key1]["response"]["results"]["result"][0]
-	zestimate = results["zestimate"]["amount"]["$"]
-	#home_details = results["homedetails"]["$"]
-	#map_home = results["mapthishome"]["$"]
-	links = results["links"]
-	home_details = links["homedetails"]["$"]
-	map_home = links["mapthishome"]["$"]
+# 	street_adrs = request.args.get("address").title()
+	
+# 	zipcode = request.args.get("zip")
+# 	citystatezip = " ".join(["Oakland,", "CA", zipcode])
+
+# 	url = "https://www.zillow.com/webservice/GetSearchResults.htm"
+# 	payload = {
+# 		"zws-id": key,
+# 		"address": street_adrs,
+# 		"citystatezip": citystatezip
+# 	}
+
+# 	# make a request to the api with the payload as parameters. Returns XML.
+# 	data = requests.get(url, params=payload) # <class requests.models.Response>
 	
 
-	# turn the dictionary into a string
-	# data_str = json.dumps(data_dict) # <class str>
-	# print("\n TTTTTTTTTTTTTT")
-	# print(type(data_str))
-
-	# data_json = jsonify(data_str)  # <class 'flask.wrappers.Response'>
-	# print("\nDDDDDDDDDDDDDDDDDDDDJJJJJJJJJJJJ")
-	# print(type(data_json))
-	# pprint(data_json)
-
-	# zestimate = data_json.results.
-	# print("\n zzzzzzzzzzzzzzzzz")
-	# print(zestimate)
+# 	# turn the XML recieved into a dictionary
+# 	data_dict = bf.data(fromstring(data.text)) # <class dict>
+	
+# 	# first key of data_dict
+# 	key1 = "{http://www.zillow.com/static/xsd/SearchResults.xsd}searchresults"
+# 	results = data_dict[key1]["response"]["results"]["result"][0]
+# 	zestimate = results["zestimate"]["amount"]["$"]
+	
+# 	links = results["links"]
+# 	home_details = links["homedetails"]["$"]
+# 	map_home = links["mapthishome"]["$"]
 	
 
-	return render_template("zillow_data.html", 
-						   zestimate=zestimate,
-						   home_details=home_details,
-						   map_home=map_home,
-						   links=links)
+# 	return render_template("zillow_data.html", 
+# 						   zestimate=zestimate,
+# 						   home_details=home_details,
+# 						   map_home=map_home,
+# 						   links=links)
 
 
 
