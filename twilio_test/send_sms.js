107ef56722514455443ba5183fc6f0a7acf262c0
stroopwafel79 <stroopwafel79@gmail.com>
2018-12-27 05:21:58 +0000

    Successfully sent textmessage with send_sms.js file

--- twilio_test/send_sms.js
+++ twilio_test/send_sms.js
@@ -1,13 +1,13 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 const accountSid = 'AC004163108d2c36a528573dbb616efcbd';
-const authToken = 'your_auth_token';
+const authToken = 'f6d505c1664c7c31bc3d10ba3a363847';
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+15017122661',
+     from: '+14083594778',
      to: '+15105520442'
    })
   .then(message => console.log(message.sid))
